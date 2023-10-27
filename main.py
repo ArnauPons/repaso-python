e998e21bb843e5bcb848bbf4925c948ff5539754
Arnau <al.arnauponsvillalonga@iesramonllull.net>
2023-10-27 12:53:02 +0200

    primer commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("hola mundo")
+#soy un comentario super guay y frutifantástico
+
+
+
+ #Variables
+
+texto= "Hola me llamo "
+nombre= "pepe antonio, mido"
+altura= "179 y nací en"
+año= "2006"
+
+print (f"{texto} {nombre} {altura} {str(año)}")
+print (texto + " - " + nombre+ " - " + altura + " - " + str(año))
+
+#Entrada
+sitioweb = input("¿cual es tu página web: ")
+print( "el siitio web del usuario es: "sitioweb)
